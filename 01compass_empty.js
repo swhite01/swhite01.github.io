a7ef68574f042922a65057e44f88ed1240abccfb
swhite01 <samuelwhiteart@gmail.com>
2018-02-13 14:30:49 -0600

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -11,9 +11,21 @@ oriented = function(e) {
     //&&=and ||=or
 
         if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = compassHeading; 
+                text_area.textContent = "north"; 
         }
-         
+
+        if(compassHeading > 45 || compassHeading < 135) {
+                text_area.textContent = "east"; 
+        }
+
+        if(compassHeading > 135 || compassHeading < 225) {
+                text_area.textContent = "south";
+        }
+
+        if(compassHeading > 225 || compassHeading < 315) {
+                text_area.textContent = "west";
+        }
+        
 
          text_area.textContent = compassHeading;
             
